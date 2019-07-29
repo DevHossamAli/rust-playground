3ef855fffb3785ee4a8011b382741c229151298e
hossam ali <dev.hossam.ali@gmail.com>
2019-07-29 17:09:16 +0200

    Tuples, Arrays, Strings & Slices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 fn main() {
     // Common premitive types in Rust:
     // Integer: i8, u8, i16, u16, i32, u32, i64, u64, isize, usize
@@ -25,7 +27,8 @@ fn main() {
     let _e = 51 % 2;
 
     // bool type
-    let _is_small: bool = false; // With defined type
+    // With type definition
+    let _is_small: bool = false;
     // char type
     let _c: char = 'x';
     // Tuple type
@@ -38,4 +41,38 @@ fn main() {
     // Accessing array element by index
     let a1 = my_arr[0];
     println!("{}", a1);
+
+    // Empty Tuple
+    let _empty_tuple = ();
+    // Note. any function that doesn't return anything actually returns empty tuple!
+    // Nesting Tuples
+    let t = (2, 's', true);
+    let t2 = (7, 'e', (1, 'a', false));
+    // {:?} tells Rustc to get the tuple value using debug flag
+    // tuple.{index} to access the tuple values
+    println!("{} {:?}", t.0, t2);
+    // Debug flag with prettier flag {:#?}
+    println!("{} {:#?}", t.0, t2);
+
+    // Array definition with type signature of i32 and length of 5
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{} {} {}", arr[1], arr.len(), mem::size_of_val(&arr));
+    // Slicing the array
+    let slice = &arr[2..4];
+    println!("Parent Array: {:?}", arr);
+    println!("Sliced Array: {:?}", slice);
+
+    // String types
+    let s1 = "String"; // Slice of string (as character sequence)
+    let str1 = s1.to_string(); // Actual String
+    let s2 = String::from("myString"); // Convert to actual string using from fn
+    println!("{} {} {}", s1, str1, s2);
+    // Take a slice form string literals
+    let str_slice = &s1[1..3];
+    println!("{}", str_slice);
+    // String concatenation
+    let h = String::from("Hello ");
+    let w = String::from("World!");
+    let hw = h + &w;
+    println!("{}", hw);
 }
